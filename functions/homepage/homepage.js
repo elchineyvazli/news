41a8625f109580b2fe7b34f304a4d31e9eefde56
e.eyvazli <elchineyvazli22@gmail.com>
2025-01-06 17:23:24 +0400

    commit

--- functions/homepage/homepage.js
+++ functions/homepage/homepage.js
@@ -61,31 +61,65 @@ lengthLimit(t_descs_for_limit);
 let editors_pick_cards = document.getElementsByClassName('e_card');
 let e_views_counts = document.getElementsByClassName('e_views_count');
 
-fetch('http://127.0.0.1:8000/api/ecards/')
-    .then(res => res.json())
-    .then(data => {
-        try {
+const get_the_card_infos = async () => {
+    const ecard_url = "http://127.0.0.1:8000/api/ecards/";
+
+    try {
+        const getResponse = await fetch(ecard_url);
+        const cardData = await getResponse.json();
+
+        if (getResponse.ok) {
             for (let i = 0; i < editors_pick_cards.length; i++) {
-                const tag_list = data[i].e_tag;
-                if (i < data.length) {
+                const tag_list = cardData[i].e_tag;
+                if (i < cardData.length) {
                     if (editors_pick_cards[i] != undefined) {
                         for (let k = 0; k < tag_list.length; k++) {
                             const tag = document.createElement('div');
-                            tag.className = `tag ${tag_list[k].toLowerCase()}`;
+                            tag.className = `tag ${tag_list[k].toLowerCase() == "food & drinks" ? "food_drinks" : tag_list[k].toLowerCase()}`;
                             tag.innerText = tag_list[k];
                             editors_pick_cards[i].children[2].appendChild(tag);
                         }
-                        editors_pick_cards[i].children[3].innerText = data[i].e_title;
-                        editors_pick_cards[i].children[4].innerText = data[i].e_desc;
-                        e_views_counts[i].innerText = data[i].e_views_count;
+                        editors_pick_cards[i].children[3].innerText = cardData[i].e_title;
+                        editors_pick_cards[i].children[4].innerText = cardData[i].e_desc;
+                        e_views_counts[i].innerText = cardData[i].e_views_count;
                     }
                 }
             }
         }
-        catch {
-            console.error("API ERROR");
-        }
-    });
+    }
+    catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+get_the_card_infos();
+
+
+// fetch('http://127.0.0.1:8000/api/ecards/')
+//     .then(res => res.json())
+//     .then(data => {
+//         try {
+//             for (let i = 0; i < editors_pick_cards.length; i++) {
+//                 const tag_list = data[i].e_tag;
+//                 if (i < data.length) {
+//                     if (editors_pick_cards[i] != undefined) {
+//                         for (let k = 0; k < tag_list.length; k++) {
+//                             const tag = document.createElement('div');
+//                             tag.className = `tag ${tag_list[k].toLowerCase() == "food & drinks" ? "food_drinks" : tag_list[k].toLowerCase()}`;
+//                             tag.innerText = tag_list[k];
+//                             editors_pick_cards[i].children[2].appendChild(tag);
+//                         }
+//                         editors_pick_cards[i].children[3].innerText = data[i].e_title;
+//                         editors_pick_cards[i].children[4].innerText = data[i].e_desc;
+//                         e_views_counts[i].innerText = data[i].e_views_count;
+//                     }
+//                 }
+//             }
+//         }
+//         catch {
+//             console.error("API ERROR");
+//         }
+//     });
 
 //#endregion
 
@@ -140,15 +174,71 @@ for (let i = 0; i < e_cards.length; i++) {
     e_cards[i].style.height = `${e_cards_video[0].clientHeight}px`;
 }
 
+document.addEventListener('click', (e) => {
+    if (
+        e.target.className != "e_card"
+        &&
+        e.target.className != "e_card_video"
+        &&
+        e.target.className != "e_about_tag"
+        &&
+        e.target.className != "play_button fa-solid fa-circle-pause"
+        &&
+        e.target.className != "e_card_h1"
+        &&
+        e.target.className != "e_card_p"
+        &&
+        e.target.className != "e_card_inf_con"
+        &&
+        e.target.className != "e_card_inf_con1"
+        &&
+        e.target.className != "e_card_inf_con2"
+        &&
+        e.target.className != "e_pp"
+        &&
+        e.target.className != "e_name"
+        &&
+        e.target.className != "fa-regular fa-eye"
+        &&
+        e.target.className != "e_views_count"
+    ) {
+        for (let i = 0; i < e_cards_video.length; i++) {
+            if (play_buttons[i].className = "play_button fa-solid fa-circle-pause") {
+                e_cards_video[i].pause();
+                play_buttons[i].className = "play_button fa-solid fa-circle-play";
+            }
+        }
+    }
+});
+
+
 for (let i = 0; i < play_buttons.length; i++) {
+    play_buttons[i].addEventListener('click', function () {
+        if (play_buttons[i].className.includes("fa-circle-play")) {
+            e_cards_video[i].play();
+            play_buttons[i].className = "play_button fa-solid fa-circle-pause";
+
+            for (let j = 0; j < play_buttons.length; j++) {
+                if (i != j) {
+                    e_cards_video[j].pause();
+                    play_buttons[j].className = "play_button fa-solid fa-circle-play";
+                }
+            }
+
+        } else {
+            e_cards_video[i].pause();
+            play_buttons[i].className = "play_button fa-solid fa-circle-play";
+        }
+    });
+
+
     e_cards_video[i].addEventListener('ended', () => {
         e_cards_video[i].play();
         const fetchAndUpdateViews = async (cardId) => {
-            const getUrl = `http://127.0.0.1:8000/api/ecards/${cardId}/`;
-            const patchUrl = `http://127.0.0.1:8000/api/ecards/${cardId}/`;
+            const dynamic_url = `http://127.0.0.1:8000/api/ecards/${cardId}/`;
 
             try {
-                const getResponse = await fetch(getUrl);
+                const getResponse = await fetch(dynamic_url);
                 const cardData = await getResponse.json();
 
                 if (getResponse.ok) {
@@ -157,7 +247,7 @@ for (let i = 0; i < play_buttons.length; i++) {
 
                     const data = { e_views_count: updatedViewsCount };
 
-                    const patchResponse = await fetch(patchUrl, {
+                    const patchResponse = await fetch(dynamic_url, {
                         method: 'PATCH',
                         headers: {
                             'Content-Type': 'application/json',
@@ -170,8 +260,9 @@ for (let i = 0; i < play_buttons.length; i++) {
                         console.log('Card updated successfully:', patchResult); //! Bu silinecek
                         e_views_counts[i].innerText = updatedViewsCount;
                     } else {
-                        console.error('Error updating card:', patchResult);
+                        console.error('Error updating card:', patchResult); //! Bu silinecek
                     }
+
                 } else {
                     console.error('Error fetching card data:', cardData);
                 }
